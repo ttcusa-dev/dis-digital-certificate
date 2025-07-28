997e8c187c0f1abd375fce1aa6beb63edf5f1978
Kelvin Rodriguez <ttcusa.developers@gmail.com>
2025-07-28 17:31:32 -0400

    ....

---
+++ src/components/templates/17.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="product-info">
+    <div class="specs-grid">
+      <div class="spec-item">
+        <div class="spec-value value">{{ props.certificate.JewelryType }}</div>
+        <div class="spec-label label">STYLE</div>
+      </div>
+      <div class="spec-item">
+        <div class="spec-value value">{{ props.certificate.Metal }}</div>
+        <div class="spec-label">METAL</div>
+      </div>
+    </div>
+  </div>
+  <hr class="divider" />
+  <div class="specs-container">
+    <div
+      class="gem-center"
+      style="display: flex; flex-direction: column; align-items: center"
+    >
+      <img
+        src="../../assets/images/emerlad.svg"
+        class="gem-image"
+        alt="Emerald"
+      />
+      <div class="gem-type">
+        <div class="value">{{ props.certificate.MainStoneType }}</div>
+        <div class="round label">({{ props.certificate.MainStoneShape }})</div>
+        <div style="margin-top: 20px">
+          <div class="specs-value value">
+            {{ props.certificate.MainStoneMeasurements }}
+          </div>
+          <div class="specs-label">GEM SIZE</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(["certificate"]);
+</script>
+
+<style>
+</style>
---
+++ src/components/templates/20.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="product-info">
+    <div class="specs-grid">
+      <div class="spec-item">
+        <div class="spec-value value">{{ props.certificate.JewelryType }}</div>
+        <div class="spec-label label">STYLE</div>
+      </div>
+      <div class="spec-item">
+        <div class="spec-value value">{{ props.certificate.Metal }}</div>
+        <div class="spec-label">METAL</div>
+      </div>
+    </div>
+  </div>
+
+  <div class="primary-gem">
+    <div class="spec-label">PRIMARY:GEM STONE</div>
+    <div class="gem-content-layout">
+      <div class="specs-container">
+        <div class="size-info">
+          <div class="specs-value value">
+            {{ props.certificate.MainStoneMeasurements }}
+          </div>
+          <div class="specs-label">GEM SIZE</div>
+        </div>
+        <div
+          class="gem-center"
+          style="display: flex; flex-direction: column; align-items: center"
+        >
+          <img
+            src="../../assets/images/emerlad.svg"
+            class="gem-image"
+            alt="Emerald"
+          />
+          <div class="gem-type">
+            <div class="value">{{ props.certificate.MainStoneType }}</div>
+            <div class="round label">({{ props.certificate.MainStoneShape }})</div>
+          </div>
+        </div>
+
+        <div class="specs-info">
+          <div class="specs-divs">
+            <div class="specs-value value"></div>
+            <div class="specs-label"></div>
+          </div>
+          <div class="specs-divs">
+            <div class="specs-value value"></div>
+            <div class="specs-label"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="primary-gem">
+    <div class="spec-label">SIDE STONE:GEM STONE</div>
+    <div class="gem-content-layout">
+      <div class="specs-container">
+        <div class="size-info">
+          <div class="specs-value value">
+            {{ props.certificate.MainStoneMeasurements }}
+          </div>
+          <div class="specs-label">GEM SIZE</div>
+        </div>
+        <div
+          class="gem-center"
+          style="display: flex; flex-direction: column; align-items: center"
+        >
+          <img
+            src="../../assets/images/emerlad.svg"
+            class="gem-image"
+            alt="Emerald"
+          />
+          <div class="gem-type">
+            <div class="value">{{ props.certificate.MainStoneType }}</div>
+            <div class="round label">({{ props.certificate.MainStoneShape }})</div>
+          </div>
+        </div>
+
+        <div class="specs-info">
+          <div class="specs-divs">
+            <div class="specs-value value"></div>
+            <div class="specs-label"></div>
+          </div>
+          <div class="specs-divs">
+            <div class="specs-value value"></div>
+            <div class="specs-label"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(["certificate"]);
+</script>
+
+<style>
+</style>
